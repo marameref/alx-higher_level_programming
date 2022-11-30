45b8426aa09e39222eff8067e345e1d3e0ee4fdb
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-11-29 17:02:34 -0800

    Task 13

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
-i = 122
-while i >= 97:
-    flag = 0
-    if i % 2 != 0:
-        i = i - 32
-        flag = 1
-    print("{:s}".format(chr(i)), end="")
-    if flag == 1:
-        i = i + 32
-    i = i - 1
+for alpha in range(122, 96, -1):
+    if alpha % 2 != 0:
+        alpha = alpha - 32
+    print("{}".format(chr(alpha)), end='')
