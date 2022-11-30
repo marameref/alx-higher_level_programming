fa2e5af2e69f1ddffd1b5febd0c8faf47be0d4ac
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-11-29 17:13:24 -0800

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(122, 96, -1):
-    if alpha % 2 != 0:
-        alpha = alpha - 32
-    print("{}".format(chr(alpha)), end='')
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
